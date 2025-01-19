a3e1da5039f5bbb738f9bc95f05babc99810d318
fabianrbn <fabianRBN_95@hotmail.com>
2025-01-19 10:17:42 -0500

    FTO: Creacion de pantalla de reportes - Inicial

--- Pichincha-Frontend/src/app/views/report/report.component.ts
+++ Pichincha-Frontend/src/app/views/report/report.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal,ViewChild  } from '@angular/core';
 
+import { ClientService } from '../../core/services/client/client.service';
+import { Client } from '../../core/interfaces/client.interface';
+import { AccountService } from '../../core/services/account/account.service';
+import { Account } from '../../core/interfaces/account.interface';
+import { FormsModule ,FormControl , FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'; 
+import { MovementService } from '../../core/services/movement/movement.service';
+import { Movement } from '../../core/interfaces/movement.interface';
+import { AccountMovement } from '../../core/interfaces/accountMovement.interface';
 @Component({
   selector: 'app-report',
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './report.component.html',
-  styleUrl: './report.component.css'
+  styleUrl: './report.component.css',
+  providers:[ClientService,AccountService,MovementService]
 })
 export class ReportComponent {
+  private clientService = inject(ClientService)
+  private accounService = inject(AccountService)
+  private movementService = inject(MovementService)
 
+  idTitular:string="";
+  clientTitular: Client= {
+    id: 0,
+    name: '',
+    address: '',
+    gender: '',
+    age: 0,
+    phoneNumber: '',
+    status: false,
+    password: '',
+    identification: ''
+  }
 }
